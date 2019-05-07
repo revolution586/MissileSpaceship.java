b928431165e10539da64f70d65dec652e7c6325a
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-07 19:04:56 -0400

    Update Missile.java

--- Missile.java
+++ Missile.java
@@ -1,3 +1,32 @@
 package MissileSpaceShip;
 
-public 
+public class Missile extends Sprite {
+  
+  private final int BOARD_WIDTH = 390;
+  private final int MISSILE_SPEED = 2;
+  
+  public Missile(int x, int y) {
+    super(x, y);
+    
+    initMissile();
+  }
+  
+  private void initMissile() {
+    
+    loadImage("src/resources/missile.png");
+    getImageDimensions();
+  }
+  
+  public void move() {
+    
+    x += MISSILE_SPEED;
+    
+    if (x > BOARD_WIDTH) {
+      visible = false;
+    }
+  }
+}
+//Here we havve a new sprite called Missile//
+//The missile moves at constant speed//
+//When it hits the right border of the Board,it becomes invisible//
+//It is then removed from the list of missiles//
